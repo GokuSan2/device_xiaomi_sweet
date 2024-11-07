6687462fa2798ef8f8cc6aac7d4b22544034a2db
kamleshhv <kamleshvansajalia@gmail.com>
2024-11-07 13:40:02 +0530

    sweet: welcome genesisos
    Signed-off-by: kamleshhv <kamleshvansajalia@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,19 +1,19 @@
 echo 'cloning sm6150-common'
-git clone -b pixel https://github.com/kamleshhv/device_xiaomi_sm6150-common.git device/xiaomi/sm6150-common
+git clone -b 15 https://github.com/kamleshhv/device_xiaomi_sm6150-common.git device/xiaomi/sm6150-common
 
 echo 'cloning vendor'
-git clone -b pixel https://github.com/kamleshhv/vendor_xiaomi_sweet.git vendor/xiaomi/sweet
+git clone -b 15 https://github.com/kamleshhv/vendor_xiaomi_sweet.git vendor/xiaomi/sweet
 
 echo 'cloning sm6150-common'
-git clone -b pixel https://github.com/kamleshhv/vendor_xiaomi_sm6150-common.git vendor/xiaomi/sm6150-common
+git clone -b 15 https://github.com/kamleshhv/vendor_xiaomi_sm6150-common.git vendor/xiaomi/sm6150-common
 
 echo 'cloning kernel'
-git clone -b fourteen https://github.com/unmoved21/kernel_xiaomi_sweet.git --depth=1 kernel/xiaomi/sm6150
+git clone -b sleepy https://github.com/kamleshhv/kernel_xiaomi_sm6150.git --depth=1 kernel/xiaomi/sm6150
 
 echo 'cloning miuicamera'
 git clone -b fourteen https://github.com/PixelOS-Devices/vendor_xiaomi_sweet-miuicamera.git vendor/xiaomi/sweet-miuicamera
 
 echo 'hardware/xiaomi'
 rm -rf hardware/xiaomi
-git clone -b 14 https://github.com/kamleshhv/android_hardware_xiaomi.git hardware/xiaomi
+git clone -b 15 https://github.com/kamleshhv/android_hardware_xiaomi.git hardware/xiaomi
 
